5da6f43113c711c4b15ee55215143c5057a2d372
snwy <snwy@snwy.me>
2023-04-12 16:51:33 -0400

    .

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,79 @@
 <script>
-	export let name;
+	import GridElement from "./GridElement.svelte";
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+		get: (searchParams, prop) => searchParams.get(prop),
+	});
+	export let name = params.username || "";
+	export let replitname = "";
+	export let grid_data = [];
+	export let most_commits = [];
+	export let all_commits = [];
+	export let bigname = "";
+	export let sum = 0;
+	export let days = 0;
+	export let average = 0;
+	export let percentage = 0;
+
+	fetch(`http://localhost:8090/users/${name}`).then(d => d.json()).then(d => {
+		grid_data = d.grid_data;
+		most_commits = d.most_commits;
+		all_commits = d.all_commits;
+		bigname = d.full_name;
+		sum = d.sum;
+		days = d.days;
+		average = d.average;
+		percentage = d.percentage;
+		replitname = d.replitname;
+	});
+
+	const cyrb53 = (str, seed = 0) => {
+		let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+		for(let i = 0, ch; i < str.length; i++) {
+			ch = str.charCodeAt(i);
+			h1 = Math.imul(h1 ^ ch, 2654435761);
+			h2 = Math.imul(h2 ^ ch, 1597334677);
+		}
+		h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+		h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+		h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+		h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+		return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+	};
+
+	String.prototype.hashCode = function() {
+		return cyrb53(this);
+	}
+
 </script>
 
 <style>
 	main {
 		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+		margin: 48px auto 0;
 		display: flex;
+		justify-content: space-around;
 	}
 
-	table {
-		border-collapse: collapse;
-	}
-	tbody, th, td {
-		border: 1px solid black;
-		margin: 16px;
-	}
-
-	td {
+	.commitBlock {
+		display: flex;
+/*		background-color: #f9f9f9;*/
 		padding: 8px;
-		border: 1px solid black;
+		border-radius: 8px;
+		vertical-align: center;
+		flex-direction: row;
+		color: #1f2d3d;
+		box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);
+	}
+
+	#feed {
+		list-style: none;
+		padding: 0;
+	}
+
+	h1 {
+		font-size: xxx-large;
 	}
 
 	@media (min-width: 640px) {
@@ -31,92 +84,84 @@
 </style>
 
 <main>
-	<div style="float: left">
-	<textarea placeholder="Github Username" bind:value={name}></textarea>
-	<button on:click={async () => {
-		// fetch commits + dates of commits from github
-		// display them in a table
-	    let data = [];
-	    await fetch(`https://api.github.com/users/${name}/events/public?per_page=90`).then(res => res.json()).then(data_from => {
-			data_from.filter(event => event.type === 'PushEvent')
-			.map(event => {
-				const date = new Date(event.created_at);
-				data.push({
-					date: date.toLocaleDateString(),
-					commits: event.payload.commits,
-					event: event
-				});
-			})
-		});
-		console.log(data);
-		for (let i = 0; i < data.length; i++) {
-			// combine commits on the same day
-			if (data[i].date === data[i+1]?.date) {
-				data[i].commits = data[i].commits.concat(data[i+1].commits);
-				data.splice(i+1, 1);
-				i--;
-			}
-		}
-		// insert psuedo-commits representing days without commits
-		for (let i = 0; i < data.length; i++) {
-			const date = new Date(data[i].date);
-			const next_date = new Date(data[i+1]?.date);
-			let difference = next_date ? (date - next_date) / (1000 * 3600 * 24) : 0;
-			date.setDate(date.getDate() + 1);
-			if (next_date && difference > 1) {
-				data.splice(i+1, 0, {
-					date: date.toLocaleDateString(),
-					commits: [
-						{
-							message: '<strong>No commits on this day</strong>'
-						}
-					],
-					event: {
-						repo: {
-							name: 'No Commits'
-						}
-					},
-					tag: "ignore"
-				});
-				i++;
-			}
-		}
-		// sort by date
-		data.sort((a, b) => new Date(b.date) - new Date(a.date));
-		// display in table
-		document.getElementById('stats').innerHTML =
-			data.map(d => `<tr><td>${d.date}</td> <td>${d.commits.map(e => `${d.event.repo.name} : ${e.message.length > 50 ? e.message.substr(0, 50) + '...' : e.message}`).join('<br>')}</td></tr>`).join('');
+	<div>
+		<div style="display: flex; align-items: center; margin-bottom: 32px">
+			<img width="96" height="96" src={`https://github.com/${name}.png`} style="border-radius: 8px; box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);">
+			<div style="margin-left: 16px">
+				<h1 style="font-size: xx-large; margin-bottom: 3%">{bigname}</h1>
+				<div style="display: flex">
+					<div style="display: flex; ">
+						<i class="fa fa-github" style="font-size: 24px">
 
-		// sum commits without ignore tag
-		const sum = data.filter(d => !d.tag).reduce((a, b) => a + b.commits.length, 0);
-		// days with commits without ignore tag
-		const days = data.filter(d => !d.tag).length;
-		// calculate average commits per day
-		const average = sum / data.length;
-		// calculate perecentage of 90 days with commits
-		const percentage = sum / 90 * 100;
-		// display stats
-		document.getElementById('sum').innerHTML = `Total commits this season: ${sum}`;
-		document.getElementById('days').innerHTML = `Total days with commits: ${days}`;
-		document.getElementById('avg').innerHTML = `Average commits per day: ${average.toFixed(2)}`;
-		document.getElementById('percent').innerHTML = `Percentage of days with commits: ${percentage.toFixed(2)}%`;
-	}}>Submit</button>
-	<!-- 18x5 grid -->
-	<table>
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Commits</th>
-			</tr>
-		</thead>
-		<tbody id="stats">
-		</tbody>
-	</table>
+						</i>
+						<a href="https://github.com/{name}" style="margin-left: 8px; color: black; font-size: 16px">github</a>
+					</div>
+
+					{#if replitname !== null && replitname !== "" && replitname !== undefined}
+
+						<div style="display: flex; margin-left: 8px">
+							<span style="font-size: 20px">⠕</span><a href="https://replit.com/@{replitname}" style="margin-left: 4px; color: black; font-size: 16px">replit</a>
+						</div>
+					{/if}
+				</div>
+			</div>
+		</div>
+		<!-- 18x5 grid -->
+		<table style="background-color: #f9f9f9; border-radius: 8px; padding-top: 8px; padding-left: 4px; padding-right: 4px; box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);">
+			<tbody id="stats">
+			{#each grid_data as row}
+				<tr style="vertical-align: middle;">
+					{#each row as col}
+						<td style="text-align: center; vertical-align: middle;"><GridElement date={col.date} commits={col.commits}></GridElement> </td>
+					{/each}
+				</tr>
+			{/each}
+			</tbody>
+		</table>
+		<div style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 2%;" id="repolist">
+			{#each most_commits as repo, i}
+				<a href="https://github.com/{repo[0]}" style="color: black; text-decoration: none">
+
+				<div class="commitBlock" style="display: block; background-color: hsl({repo[0].hashCode() % 360}, 70%, 95%">
+					<span style="font-size: x-large">{i + 1}. </span>
+					<strong>{repo[0]}</strong>
+					<br>
+					{repo[1]} days with commits
+					</div></a>
+			{/each}
+		</div>
 	</div>
-	<div style="float: right">
-		<h1 id="sum">Total commits this season: </h1>
-		<h1 id="days">Total days with commits: </h1>
-		<h1 id="avg">Average commits per day: </h1>
-		<h1 id="percent">Percentage of days with commits: </h1>
+	<div>
+		<div style="display: grid; line-height: 32pt; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr 1fr; margin: 16px">
+			<div style="display: flex; flex-direction: column; text-align: center; margin: 8px">
+				<h1 id="sum">{sum}</h1>
+				<h2>total commits</h2>
+			</div>
+			<div style="display: flex; flex-direction: column; text-align: center; margin: 8px">
+				<h1 id="days">{days}</h1>
+				<h2>days with commits</h2>
+			</div>
+			<div style="display: flex; flex-direction: column; text-align: center; margin: 8px">
+				<h1 id="avg">{average.toPrecision(3)}</h1>
+				<h2>avg. commits/day</h2>
+			</div>
+			<div style="display: flex; flex-direction: column; text-align: center; margin: 8px">
+				<h1 id="percent">{percentage.toPrecision(2)}%</h1>
+				<h2>of days have commits</h2>
+			</div>
+		</div>
+		<ul id="feed">
+			{#each (() => {
+				let result = all_commits.sort((a, b) => {
+					return new Date(b.date) - new Date(a.date);
+				});
+				return result;
+			})() as commit}
+				<li><a href="http://github.com/{commit.event.repo.name}/commit/{commit.event.payload.commits[0].sha}" style="text-decoration: none"><div class="commitBlock" style="background-color: #f9f9f9"><div class="material-symbols-outlined" style="margin-right: 2%; margin-left: 2%">
+commit
+				</div><div>{commit.event.repo.name}</div><div style="margin-left: auto">{new Date(commit.date).toLocaleDateString()}</div></div></a></li>
+			{/each}
+		</ul>
 	</div>
+
 </main>
--- src/GridElement.svelte
+++ src/GridElement.svelte
@@ -1,23 +1,70 @@
+<style>
+    /* Tooltip container */
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        /* border-bottom: 1px dotted black; If you want dots under the hoverable text */
+    }
+
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: max-content;
+        min-width: 100px;
+        padding: 4px;
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+
+        /* Position the tooltip text - see examples below! */
+        position: absolute;
+        z-index: 1;
+
+        top: 125%;
+        left: 50%;
+        margin-left: -60px; /* Use half of the width (120/2 = 60), to center the tooltip */
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+    }
+
+    .gridItem {
+        width: 16px;
+        height: 16px;
+        border-radius: 4px;
+        margin: 2px;
+    }
+</style>
+
 <script>
     export let date
     export let commits = []
-    /* schema for commits
-        {
-            repo: string,
-            message: string,
-        }
-     */
 
-     // on hover, show a bubble with the date + commits for that day
-    function showCommits() {
-        let bubble = document.getElementById('bubble')
-        bubble.style.display = 'block'
-        bubble.style.left = event.clientX + 'px'
-        bubble.style.top = event.clientY + 'px'
-        bubble.innerHTML = `<h3>${date}</h3>`
-        for (let commit of commits) {
-            bubble.innerHTML += `<p>${commit.repo}: ${commit.message}</p>`
+    // combine commits in the same repo
+    let commitsByRepo = {}
+    commits.forEach(commit => {
+        if (!commitsByRepo[commit.repo]) {
+            commitsByRepo[commit.repo] = []
         }
+        commitsByRepo[commit.repo].push(commit)
+    })
+
+    let tooltip_array = []
+    for (let repo in commitsByRepo) {
+        tooltip_array.push(`${commitsByRepo[repo].length} commits in ${repo} on ${date}`)
     }
-
+    export function getTooltips() {
+        return commits.length === 0 ? [`No commits on ${date}`] : tooltip_array
+    }
 </script>
+
+<div class="gridItem tooltip" style="background-color: {commits.length === 0 ? `var(--slate)` : `var(--green)`}">
+        <span class="tooltiptext">
+        {#each getTooltips() as tooltip}
+            {tooltip}<br>
+        {/each}
+        </span>
+</div>
